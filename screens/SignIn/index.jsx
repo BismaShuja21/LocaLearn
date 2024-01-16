9d1c394c66e7f2bdeb648400aab9ed5ec4af2869
Bisma Shuja <bismashuja2133@gmail.com>
2024-01-16 17:43:01 +0500

    notification setup

--- screens/SignIn/index.jsx
+++ screens/SignIn/index.jsx
@@ -72,7 +72,7 @@ export default function SignIn() {
                   label={"Sign In"}
                   onPress={() => {
                     console.log(errors);
-                    navigation.navigate("TutorTab");
+                    navigation.navigate("StudentTab");
                     // handleSubmit();
                   }}
                 />
